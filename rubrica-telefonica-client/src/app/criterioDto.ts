3d590349b1004e046b9e5327133fa91cf6a048ec
Ioana21-b <56729761+Ioana21-b@users.noreply.github.com>
2021-03-10 17:32:02 +0100

    Implementazione lato Client logica Find #7
    Implementazione lato Client logica Find #7

---
+++ rubrica-telefonica-client/src/app/criterioDto.ts
@@ -0,0 +1,3 @@
+export class CriterioDto{
+    criterio: string;
+}
--- rubrica-telefonica-client/src/app/search-page/search-page.component.ts
+++ rubrica-telefonica-client/src/app/search-page/search-page.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CriterioDto } from '../criterioDto';
+import { ListaContattiDto } from '../listaContattiDto';
+import { RubricaService } from '../rubrica-service.service';
 
 @Component({
   selector: 'app-search-page',
@@ -7,12 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPageComponent implements OnInit {
 criterio: string;
-  constructor() { }
+  constructor(private http: HttpClient, public rubrica: RubricaService) { }
 
   ngOnInit(): void {
   }
   // tslint:disable-next-line:typedef
   cerca(){
+    const dto: CriterioDto = new CriterioDto;
+    dto.criterio = this.criterio;
+    const oss: Observable<ListaContattiDto> = this.http
+    .post<ListaContattiDto>('http://localhost:8080/cerca', dto);
+    oss.subscribe(l => this.rubrica.contatti = l.listaContatti);
   }
   // tslint:disable-next-line:typedef
   rimuovi(){
